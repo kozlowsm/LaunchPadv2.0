a478ee88e672ec2ac686e8eff0fd36582603016a
Michael Kozlowski <kozlowsm@oregonstate.edu>
2019-01-05 10:42:30 -0500

    Close server gracefully on uncaughtException

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Bring in the initialized application
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
@@ -5,4 +6,12 @@ app.set('port', port);
 const server = app.listen(app.get('port'), () => {
   // eslint-disable-next-line no-console
   console.log(`Express Server running -> PORT: ${server.address().port}`);
+});
+
+// Close the server gracefully if something bad happens
+process.on('uncaughtException', err => {
+  // eslint-disable-next-line no-console
+  console.log('something terribly wrong happened', err);
+
+  server.close(() => process.exit(1));
 });
--- utilities/fetchData.js
+++ utilities/fetchData.js
@@ -1,5 +1,10 @@
 const fetch = require('node-fetch');
 
+/*******************************************************
+ * Gets Data from LaunchLibrary API
+ * Default number of launches is 10
+ * To get more/less launches provide a count argument to the function
+********************************************************/ 
 const getLaunchData = async (count = 10) => {
   const url = `https://launchlibrary.net/1.4/launch/next/${count}/?mode=verbose`;
   const res = await fetch(url);

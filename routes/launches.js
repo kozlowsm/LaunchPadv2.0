bce877a71f7c83543988339f8665bac207a33754
cberry216 <christopherberry216@gmail.com>
2019-01-16 09:43:10 -0500

    Made skeleton for individual launch pages.

--- routes/launches.js
+++ routes/launches.js
@@ -1,6 +1,17 @@
 const express = require('express');
 
 const router = express.Router();
+const { getSingleLaunchData } = require('./../utilities/fetchData');
 
-router.get('/:id', (req, res) => {});
+router.get('/:id', (req, res) => {
+  const launchID = req.params.id;
+  const launchData = getSingleLaunchData(launchID);
+  launchData
+    .then(data => {
+      res.render('launches/launch', { data });
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
+});
 module.exports = router;

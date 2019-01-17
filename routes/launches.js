d250d5b172bea901f63c8e9d69b40535ea7fec0e
kozlowsm <kozlowsm@oregonstate.edu>
2019-01-17 13:47:27 -0500

    setup individual launch page

--- routes/launches.js
+++ routes/launches.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const { getSingleLaunchData } = require('../utilities/fetchData');
 
 const router = express.Router();
 
-router.get('/:id', (req, res) => {});
+router.get('/:id', (req, res) => {
+  const launchID = req.params.id;
+  const launchData = getSingleLaunchData(launchID);
+
+  // Use the data
+  launchData
+    .then(data => {
+      res.render('launches/index', { data });
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
+});
+
 module.exports = router;
